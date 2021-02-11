7d12b9658af67b09553de3b862aada7a8032281a
Corey Miller <CoreyMiller454@gmail.com>
2021-02-11 15:56:09 -0500

    add scraper2 file with refactoring of original

--- scraper2.js
+++ scraper2.js
@@ -1,4 +1,13 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs");
+
+const storeData = (data, path) => {
+  try {
+    fs.writeFileSync(path, JSON.stringify(data));
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -15,35 +24,22 @@ const puppeteer = require("puppeteer");
 
   await autoScroll(page);
 
-  await page.screenshot({
-    path: "yoursite.png",
-    fullPage: true,
-  });
-
   const listings = await page.evaluate(() => {
     return Array.from(
-      document.querySelectorAll("div.col-2.layout-results")
+      document.querySelectorAll(".data-results-content-parent.relative")
     ).map((ele) => {
-      //Needs to get rid of /n's from array
-      let details = Array.from(ele.querySelectorAll(".data-details"))
-        .map((details) => details.textContent)
-        .map((word) => word);
-      if (
-        ele.querySelector(".data-results-title.dark-blue-text.b")
-          .textContent !== ""
-      ) {
+      {
         return {
           title: ele.querySelector(".data-results-title.dark-blue-text.b")
             .textContent,
           logo: ele.querySelector(".data-results-img img")
             ? ele.querySelector(".data-results-img img").src
             : null,
-          description: details,
         };
       }
     });
   });
-  console.log(listings);
+  storeData(listings, "listings.json");
   await browser.close();
 })();
 
